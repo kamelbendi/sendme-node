a80c21dba52bb62ffa24a0c2bb1f3a3bad574e61
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-01-28 23:51:37 +0100

    sms backend final

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
+require('express-async-errors')
+const winston = require('winston');
 const express = require('express');
-//import { setupUsersDatabase } from "./db/setup";
-const  { pool } = require("./db/db");
-const TextFlow = require("textflow.js");
-TextFlow.useKey("5IFWwEiNbn8oWDsykxRRMCCzVdMOnpx3clHg5eeZ7fB6wXc9V2JzP6uYrz62QCRZ");
 const app = express();
-const PORT = process.env.PORT || 3030;
 
-// Database setup
-//setupUsersDatabase();
+winston.add(winston.transports.File, { filename: 'logfile.log' })
+process.on('uncaughtException', (ex) => {
+    winston.error(ex.message, err)
+    process.exit(1)
+})
+
+process.on('unhandledRejection', (ex) => {
+    winston.error(ex.message, err)
+    process.exit(1)
+})
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
+
+//----- 
+
+//import { setupUsersDatabase } from "./db/setup";
+const  { pool } = require("./db/db");
+const TextFlow = require("textflow.js");
+TextFlow.useKey("5IFWwEiNbn8oWDsykxRRMCCzVdMOnpx3clHg5eeZ7fB6wXc9V2JzP6uYrz62QCRZ");
+
+// Database setup
+//setupUsersDatabase();
+
+
 // your code
 
 app.post('/verifysms', async (req, res) => {
-    console.log('verify sms');
-    console.log(req.body);
     const { code, phone } = req.body;
 
     var result = await TextFlow.verifyCode(phone, code);
-    console.log(result)
     if (result.valid) {
         return res.sendStatus(200).json({ success: true });
     } else {
@@ -29,15 +50,25 @@ app.post('/verifysms', async (req, res) => {
 })
 
 app.post('/getsms', async (req, res) => {
-    const number = req.body;
+    const number = req.body.phone;
 
-    var result = await TextFlow.sendVerificationSMS(number.phone, {});
+    var result = await TextFlow.sendVerificationSMS(number, {});
 
     if(result.ok) {
         return res.status(200).json({ success: true });
     }
     console.log(result);
     return res.status(400).json({ success: false });
+})
+
+app.post('/register', async (req, res) => {
+    const userData = req.body;
+    
+
+})
+
+app.post('/login', async (req, res) => {
+
 })
 
 // app.get('/users', async (req, res) => {
@@ -79,9 +110,3 @@ app.post('/getsms', async (req, res) => {
 //       res.status(500).send('Internal Server Error');
 //     }
 //   });
-  
-  // Start the server
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-  
