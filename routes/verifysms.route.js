0a489d6d4a369f7bb6d7e52fc0101c9e8c0ae987
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-01-28 23:53:24 +0100

    small fix

---
+++ routes/verifysms.route.js
@@ -0,0 +1,6 @@
+const TextFlow = require("textflow.js");
+const express = require('express');
+const router = express.Router();
+
+TextFlow.useKey("xg9JhUiVKxwnN1pLNwXE5F3b5jHHEuaV1DbwrManHLbEeUJj0sMBbKHWxHVpqFFR");
+
