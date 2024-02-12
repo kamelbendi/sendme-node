eff539d4bfd0962719d471c5f201ad0bebb21449
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-02-12 21:14:02 +0100

    create transactions table

--- db/schema.js
+++ db/schema.js
@@ -1,21 +1,21 @@
-const createHistoryTableForUser = async (clientID) => {
+const createHistoryTableForUsers = async () => {
   try {
     await global.pool.query(`
-      CREATE TABLE IF NOT EXISTS history_${clientID} (
-        id SERIAL PRIMARY KEY,
-        sender_id INTEGER,
-        receiver_id INTEGER,
-        amount DECIMAL,
-        date DATE
-      );
+    CREATE TABLE IF NOT EXISTS transactions (
+      id SERIAL PRIMARY KEY,
+      username_receiver VARCHAR(50),
+      title VARCHAR(50),
+      amount DECIMAL,
+      username_sender VARCHAR(50),
+      timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
     `);
+    console.log('Table "transactions" created successfully');
   } catch {
     console.error('Error creating history table');
   }
 }
 
 module.exports = {
-  createUsersTable,
-  createContactTableForUser,
-  createHistoryTableForUser,
+  createHistoryTableForUsers,
 };
