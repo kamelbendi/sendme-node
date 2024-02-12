705e055c72a3b57c28a06c4d984acc55b1192397
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-02-12 22:56:17 +0100

    adding Account Number generating functions

--- helpers/help.js
+++ helpers/help.js
@@ -1,7 +1,16 @@
+
 exports.getRandomInt = (min, max)=>{
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+exports.generateRandomDigitsNumber = (length)=>{
+    let result = '';
+    for (let i = 0; i < length; i++) {
+        result += Math.floor(Math.random() * 10);
+    }
+    return result;
 };
 
 exports.getCurrentDate=()=>{
@@ -21,3 +30,12 @@ exports.isValidFieldProvided=(arrayKeys1, arrayKeys2)=>{
 
     return true;
 };
+
+exports.generateExpiryDate=()=>{
+    const currentDate = new Date();
+    const expiryDate = new Date(currentDate.getFullYear() + 5, currentDate.getMonth());
+  
+    const formattedExpiryDate = `${(expiryDate.getMonth() + 1).toString().padStart(2, '0')}/${expiryDate.getFullYear()}`;
+    
+    return formattedExpiryDate;
+}
